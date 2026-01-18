3b4bb1b755b043e32c6c3fb228e43ddfcdeb8cd9
Quang Hy Le <Lequanghy1998@yahoo.ca>
2026-01-17 22:27:26 -0500

    chore: update config

--- svelte.config.js
+++ svelte.config.js
@@ -1,29 +1,17 @@
 // svelte.config.js
-import adapterStatic from '@sveltejs/adapter-static';
-import adapterNode from '@sveltejs/adapter-node';
+import adapter from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-
-const adapter =
-	process.env.ADAPTER === 'node'
-		? adapterNode({
-				out: 'build', // default
-				precompress: false,
-				envPrefix: 'SVELTE_',
-				polyfills: true
-			})
-		: adapterStatic({ fallback: 'index.html' });
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: vitePreprocess(),
 	kit: {
-		adapter: adapter
-		// adapter: adapter({
-		// 	out: 'build', // default
-		// 	precompress: false,
-		// 	envPrefix: 'SVELTE_',
-		// 	polyfills: true
-		// })
+		adapter: adapter({
+			out: 'build', // default
+			precompress: false,
+			envPrefix: 'SVELTE_',
+			polyfills: true
+		})
 	}
 };
 
